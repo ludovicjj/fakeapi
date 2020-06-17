21301249c1185cd456d921f93b1c4257ce1b0d17
ludovicjj <jahanlud@gmail.com>
2020-06-17 18:26:10 +0200

    init projet promise

---
+++ Promise/app.js
@@ -0,0 +1,31 @@
+let get = function(url, success, error) {
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            if(xhr.status === 200) {
+                success(xhr.responseText);
+            } else {
+                error(xhr);
+            }
+        }
+    };
+
+    xhr.open('GET', url, true);
+    xhr.send();
+};
+
+
+let getPost = function() {
+    get(
+        'https://my-json-server.typicode.com/ludovicjj/fakeapi/posts',
+        function(response) {
+            let posts = JSON.parse(response);
+        },
+        function(error) {
+            console.log(error);
+        }
+    )
+};
+
+getPost();
