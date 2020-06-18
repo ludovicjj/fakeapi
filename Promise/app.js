bee982e3f169a1493b14b297b9c89cd5f2beac15
ludovicjj <jahanlud@gmail.com>
2020-06-18 17:35:57 +0200

    Ajax with await&asynch

--- Promise/app.js
+++ Promise/app.js
@@ -1,32 +1,37 @@
-let get = function(url, success, error) {
-    let xhr = new XMLHttpRequest();
+// Fail : https://reqres.in/api/users/23
 
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4) {
-            if(xhr.status === 200) {
-                success(xhr.responseText);
-            } else {
-                error(xhr);
+let get = function(url) {
+    return new Promise(function (resolve, reject) {
+        let xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if(xhr.status === 200) {
+                    resolve(xhr.responseText);
+                } else {
+                    reject(xhr);
+                }
             }
-        }
-    };
+        };
 
-    xhr.open('GET', url, true);
-    xhr.send();
+        xhr.open('GET', url, true);
+        xhr.send();
+    });
 };
 
 
-let getPost = function() {
-    get(
-        'https://my-json-server.typicode.com/ludovicjj/fakeapi/posts',
-        function(response) {
-            let posts = JSON.parse(response);
-            console.log(posts);
-        },
-        function(error) {
-            console.log(error);
-        }
-    )
+let getComment = async function() {
+    var response = await get('https://my-json-server.typicode.com/ludovicjj/fakeapi/posts');
+    let posts = JSON.parse(response);
+    response = await get('https://my-json-server.typicode.com/ludovicjj/fakeapi/comments?postId=' + posts[0].id);
+    let comments = JSON.parse(response);
+    return comments;
 };
 
-getPost();
+getComment().then(comments => {
+    console.log(comments[0]);
+}).catch(error => {
+    console.log(error)
+});
+
+
