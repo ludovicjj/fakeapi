0b87fb4752bef3bb18213f5032a8eda39b5aeeef
ludovicjj <jahanlud@gmail.com>
2020-06-23 17:15:44 +0200

    Init navigation

--- carousel/js/main.js
+++ carousel/js/main.js
@@ -10,14 +10,14 @@ class Carousel {
         this.element = element;
         this.options = Object.assign({}, {slideToScroll: 1, slideVisible:1}, options);
         this.container = this.createDivWithClass('carousel__container');
-
+        this.currentItem = 0;
         // Get children HTML Element from this.element
         let children = [].slice.call(element.children);
 
         // Update DOM, this.element > root > container
-        let root = this.createDivWithClass('carousel');
-        root.appendChild(this.container);
-        this.element.appendChild(root);
+        this.root = this.createDivWithClass('carousel');
+        this.root.appendChild(this.container);
+        this.element.appendChild(this.root);
 
         // Update DOM, container > carousel__item > children. Return carousel__item[]
         this.items = children.map((child) => {
@@ -28,8 +28,13 @@ class Carousel {
         });
         // Update style width (container & carousel__items)
         this.setStyle();
+        // Update DOM with navigation ELEMENT
+        this.setNavigation();
     }
 
+    /**
+     * Applique les dimension aux elements
+     */
     setStyle() {
         let ratio = this.items.length / this.options.slideVisible;
         // container width
@@ -38,6 +43,33 @@ class Carousel {
         this.items.forEach((item) => {
             item.style.width = (100 / this.items.length) + '%';
         });
+    }
+
+    setNavigation() {
+        let next = this.createDivWithClass('carousel__next');
+        let prev = this.createDivWithClass('carousel__prev');
+        this.root.appendChild(next);
+        this.root.appendChild(prev);
+        next.addEventListener('click', this.next.bind(this));
+        prev.addEventListener('click', this.prev.bind(this));
+    }
+
+    next() {
+        this.goToItem(this.currentItem + this.options.slideToScroll);
+    }
+
+    prev() {
+        this.goToItem(this.currentItem - this.options.slideToScroll);
+    }
+
+    /**
+     * Deplace le carousel vers l'index desiré
+     * @param {number} index
+     */
+    goToItem(index) {
+        let translateX = (-100 / this.items.length) * index;
+        this.container.style.transform = 'translate3d(' + translateX + '%, 0, 0)';
+        this.currentItem = index;
     }
 
     /**
@@ -55,7 +87,7 @@ class Carousel {
 
 document.addEventListener('DOMContentLoaded', function () {
     new Carousel(document.querySelector('#carousel1'), {
-        slideToScroll: 3,
+        slideToScroll: 1,
         slideVisible: 3
     });
 });
