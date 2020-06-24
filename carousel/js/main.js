7f5748455e887ab0de1190823b4fde89a7aba319
ludovicjj <jahanlud@gmail.com>
2020-06-24 18:17:16 +0200

    add loop option

--- carousel/js/main.js
+++ carousel/js/main.js
@@ -1,16 +1,29 @@
 class Carousel {
 
     /**
+     * This callback is displayed as part of the Carousel class.
+     * @callback scrollCallback
+     * @param {number} index
+     */
+
+    /**
      * @param {HTMLElement} element
      * @param {Object} options
-     * @param {Object} options.slideToScroll Nombre d'element à faire défiler
-     * @param {Object} options.slideVisible Nombre d'element visible
+     * @param {number} [options.slideToScroll=1] Nombre d'element à faire défiler
+     * @param {number} [options.slideVisible=1] Nombre d'element visible
+     * @param {boolean} [options.loop=false] Enable loop scroll
      */
     constructor(element, options = {}) {
         this.element = element;
-        this.options = Object.assign({}, {slideToScroll: 1, slideVisible:1}, options);
+        this.options = Object.assign({},
+            {
+                slideToScroll: 1,
+                slideVisible:1,
+                loop: false
+            }, options);
         this.container = this.createDivWithClass('carousel__container');
         this.currentItem = 0;
+        this.scrollCallbacks = [];
         // Get children HTML Element from this.element
         let children = [].slice.call(element.children);
 
@@ -30,6 +43,8 @@ class Carousel {
         this.setStyle();
         // Update DOM with navigation ELEMENT
         this.setNavigation();
+
+        this.scrollCallbacks.forEach(cb => cb(0));
     }
 
     /**
@@ -52,6 +67,30 @@ class Carousel {
         this.root.appendChild(prev);
         next.addEventListener('click', this.next.bind(this));
         prev.addEventListener('click', this.prev.bind(this));
+
+        if (this.options.loop === true) {
+            return;
+        }
+        this.onScroll(index => {
+           if (index === 0) {
+               prev.classList.add('carousel__prev--hidden');
+           } else {
+               prev.classList.remove('carousel__prev--hidden');
+           }
+
+           if(this.items[this.currentItem + this.options.slideVisible] === undefined) {
+               next.classList.add('carousel__next--hidden');
+           } else {
+               next.classList.remove('carousel__next--hidden');
+           }
+        });
+    }
+
+    /**
+     * @param {scrollCallback} cb
+     */
+    onScroll(cb) {
+        this.scrollCallbacks.push(cb);
     }
 
     next() {
@@ -67,13 +106,20 @@ class Carousel {
      * @param {number} index
      */
     goToItem(index) {
+        if (index < 0) {
+            index = this.items.length - this.options.slideVisible;
+        } else if (index >= this.items.length || this.items[this.currentItem + this.options.slideVisible] === undefined) {
+            index = 0;
+        }
         let translateX = (-100 / this.items.length) * index;
         this.container.style.transform = 'translate3d(' + translateX + '%, 0, 0)';
+        // Update currentItem
         this.currentItem = index;
+        this.scrollCallbacks.forEach(callback => callback(index));
     }
 
+
     /**
-     *
      * @param {string} className
      * @returns {HTMLElement}
      */
@@ -88,6 +134,12 @@ class Carousel {
 document.addEventListener('DOMContentLoaded', function () {
     new Carousel(document.querySelector('#carousel1'), {
         slideToScroll: 1,
-        slideVisible: 3
+        slideVisible: 3,
+    });
+
+    new Carousel(document.querySelector('#carousel2'), {
+        slideToScroll: 1,
+        slideVisible: 3,
+        loop: true
     });
 });
