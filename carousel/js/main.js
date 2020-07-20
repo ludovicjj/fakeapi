6dc013e0e52ffc5182ad1b7324990f2fc21cb24b
ludovicjj <jahanlud@gmail.com>
2020-07-20 14:50:56 +0200

    responsive mobile and keyboard navigation

--- carousel/js/main.js
+++ carousel/js/main.js
@@ -24,31 +24,42 @@ class Carousel {
         let children = [].slice.call(element.children);
         this.currentSlide = 0;
         this.moveCallbacks = [];
+        this.isMobile = false;
 
         // DOM
         this.root = this.createDivWithClass('carousel');
         this.container = this.createDivWithClass('carousel__container');
         this.root.appendChild(this.container);
         this.element.appendChild(this.root);
-
+        this.root.setAttribute('tabindex', '0');
         this.items = children.map(child => {
             let item = this.createDivWithClass('carousel__item');
             item.appendChild(child);
             this.container.appendChild(item);
             return item;
         });
-
-        // Method
         this.setStyle();
         this.createNavigation();
+
+        // Event
         this.moveCallbacks.forEach(cb => cb(0));
+        this.onWindowResize(); // display mobile design on page loading
+        window.addEventListener('resize', this.onWindowResize.bind(this));
+        this.root.addEventListener('keyup', (e) => {
+            if (e.key === 'ArrowRight' || e.key === 'Right') {
+                this.next();
+            }
+            if (e.key === 'ArrowLeft' || e.key === 'Left') {
+                this.prev();
+            }
+        })
     }
 
     /**
      * Dimension des items du carousel
      */
     setStyle() {
-        let ratio = this.items.length / this.options.slideVisible;
+        let ratio = this.items.length / this.slideVisible;
         this.container.style.width = ratio * 100 + '%';
         this.items.forEach(item => item.style.width = 100 / this.items.length + '%');
     }
@@ -71,7 +82,7 @@ class Carousel {
                 prevButton.classList.remove('carousel__prev--hidden');
             }
 
-            if (this.hasItem()) {
+            if (this.hasNextItem()) {
                 nextButton.classList.add('carousel__next--hidden');
             } else {
                 nextButton.classList.remove('carousel__next--hidden');
@@ -80,11 +91,11 @@ class Carousel {
     }
 
     next() {
-        this.goToSlide(this.currentSlide + this.options.slideToScroll);
+        this.goToSlide(this.currentSlide + this.slideToScroll);
     }
 
     prev() {
-        this.goToSlide(this.currentSlide - this.options.slideToScroll);
+        this.goToSlide(this.currentSlide - this.slideToScroll);
     }
 
     /**
@@ -92,11 +103,20 @@ class Carousel {
      */
     goToSlide(slide) {
         if (slide < 0) {
-            slide = this.items.length - this.options.slideVisible;
+            if (this.options.loop) {
+                slide = this.items.length - this.slideVisible;
+            } else {
+                return;
+            }
         } else if (
-            slide >= this.items.length || this.isOverLimit(slide)
+            slide >= this.items.length ||
+            (this.items[this.currentSlide + this.slideVisible] === undefined && slide > this.currentSlide)
         ) {
-            slide = 0;
+            if (this.options.loop) {
+                slide = 0;
+            } else {
+                return;
+            }
         }
         let translateX = slide * (-100 / this.items.length);
         this.container.style.transform = 'translate3d(' + translateX + '%, 0, 0)';
@@ -105,22 +125,13 @@ class Carousel {
         this.moveCallbacks.forEach(cb => cb(slide));
     }
 
-    /**
-     * confirm carousel is over the limit
-     * @param {number} slide
-     * @returns {boolean}
-     */
-    isOverLimit(slide) {
-        let index = this.currentSlide + this.options.slideVisible;
-        return this.items[index] === undefined && slide > this.items.length - this.options.slideVisible;
-    }
 
     /**
-     * confirm item exist by index
+     * Looking if Carousel has next item by index
      * @returns {boolean}
      */
-    hasItem() {
-        return this.items[this.currentSlide + this.options.slideVisible] === undefined;
+    hasNextItem() {
+        return this.items[this.currentSlide + this.slideVisible] === undefined;
     }
 
     /**
@@ -129,6 +140,15 @@ class Carousel {
      */
     onMove(cb) {
         this.moveCallbacks.push(cb)
+    }
+
+    onWindowResize() {
+        let mobile = window.innerWidth < 800;
+        if (mobile !== this.isMobile) {
+            this.isMobile = mobile;
+            this.setStyle();
+            this.moveCallbacks.forEach(cb => cb(this.currentSlide));
+        }
     }
 
     /**
@@ -140,18 +160,39 @@ class Carousel {
         div.setAttribute('class', className);
         return div;
     }
+
+    /**
+     * @returns {number}
+     */
+    get slideToScroll() {
+        return this.isMobile ? 1 : this.options.slideToScroll;
+    }
+
+    /**
+     * @returns {number}
+     */
+    get slideVisible() {
+        return this.isMobile ? 1 : this.options.slideVisible;
+    }
 }
 
 let onReady = function () {
     new Carousel(document.querySelector('#carousel1'), {
-        slideToScroll: 1,
-        slideVisible: 3
+        slideToScroll: 3,
+        slideVisible: 3,
+        loop: true
     });
 
     new Carousel(document.querySelector('#carousel2'), {
         slideToScroll: 2,
         slideVisible: 2,
-        loop: true
+        loop: false
+    });
+
+    new Carousel(document.querySelector('#carousel3'), {
+        slideToScroll: 1,
+        slideVisible: 1,
+        loop: false
     });
 };
 
